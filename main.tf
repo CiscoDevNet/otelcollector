3ea55dfea75820807579ebf7e45e9646c36509e1
prathjan <prathjan@cisco.com>
2022-04-05 13:16:51 -0700

    ver 3

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ variable "ikswsname" {
 resource helm_release otelcollector {
   name       = "otelcollector"
   namespace = "default"
-  chart = "https://prathjan.github.io/helm-chart/opentelemetry-collector-0.12.2.tgz"
+  chart = "https://prathjan.github.io/helm-chart/opentelemetry-collector-0.12.3.tgz"
 }
 
 provider "helm" {
